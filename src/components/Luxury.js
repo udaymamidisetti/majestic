90bd179139de9e4f451bc0766e1fca4911c6c416
Nikhil Jadhav <jnikhil652@gmail.com>
2023-03-13 16:06:49 +0530

    update

--- src/components/Luxury.js
+++ src/components/Luxury.js
@@ -4,6 +4,18 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Luxury = () => {
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const formDataObj = Object.fromEntries(formData.entries());
+      let url = "https://propsource.project-test.online/";
+      formDataObj["project"] = "Majestique Enquiry Form";
+      await axios.post(url, formDataObj);
+      console.log(formDataObj, "formDataObj");
+    } catch (error) {
+    }
+  }
   const slides = [
     {
       id: 1,
@@ -126,50 +138,53 @@ const Luxury = () => {
           </div>
         </div>
       </Slider>
-      <div className="container h-[13.9375rem] m-auto max-xl:h-[27.5625rem]">
-        <div className="gap-[0.625rem] flex justify-center flex-wrap font-poppins mt-[1.625rem]">
-          <h1 className="font-semibold text-[1rem] font-poppins md:hidden">
-            Register Here and awail Best Offers
-          </h1>
-          <input
-            className="w-full max-w-[25.0625rem] h-[2.5rem] border-[0.03125rem] border-[#000000] p-[0.625rem] rounded-md"
-            placeholder="Name"
-          />
-          <input
-            className=" w-full max-w-[25.0625rem] h-[2.5rem] border-[0.03125rem] border-[#000000] p-[0.625rem] rounded-md"
-            placeholder="Mobile Number"
-          />
-          <input
-            className="w-full max-w-[25.0625rem] h-[2.5rem] border-[0.03125rem] border-[#000000] p-[0.625rem] rounded-md"
-            placeholder="Email"
-          />
-          <select
-            id="country"
-            name="country"
-            autoComplete="country-name"
-            className="w-full text-[#000000B2] text-[1rem] mt-1 block max-w-[25.0625rem] h-[2.5rem] rounded-md border border-gray-900 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-          >
-            <option className="text-[#000000B2]">
-              Apartment Interested In
-            </option>
-            <option></option>
-            <option></option>
-          </select>
-          <select
-            id="country"
-            name="country"
-            autoComplete="country-name"
-            className="w-full text-[#000000B2] text-[1rem] mt-1 block max-w-[25.0625rem] h-[2.5rem] rounded-md border border-gray-900 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-          >
-            <option className="text-[#000000B2]">Purpose Of Enquiry</option>
-            <option></option>
-            <option></option>
-          </select>
+      <form onSubmit={handleSubmit}>
+        <div className="container h-[13.9375rem] m-auto max-xl:h-[27.5625rem]">
+          <div className="gap-[0.625rem] flex justify-center flex-wrap font-poppins mt-[1.625rem]">
+            <h1 className="font-semibold text-[1rem] font-poppins md:hidden">
+              Register Here and awail Best Offers
+            </h1>
+            <input
+              name="name"
+              className="w-full max-w-[25.0625rem] h-[2.5rem] border-[0.03125rem] border-[#000000] p-[0.625rem] rounded-md"
+              placeholder="Name"
+            />
+            <input
+              name="mobile"
+              className=" w-full max-w-[25.0625rem] h-[2.5rem] border-[0.03125rem] border-[#000000] p-[0.625rem] rounded-md"
+              placeholder="Mobile Number"
+            />
+            <input
+              name="email"
+              className="w-full max-w-[25.0625rem] h-[2.5rem] border-[0.03125rem] border-[#000000] p-[0.625rem] rounded-md"
+              placeholder="Email"
+            />
+            <select
+              name="Apartment"
+              className="w-full text-[#000000B2] text-[1rem] mt-1 block max-w-[25.0625rem] h-[2.5rem] rounded-md border border-gray-900 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            >
+              <option className="text-[#000000B2]">
+                Apartment Interested In
+              </option>
+              <option value={"1 BHK"}>1 BHK</option>
+              <option value={"1 BHK"}>2 BHK</option>
+              <option value={"1 BHK"}>3 BHK</option>
+            </select>
+            <select
+              name="Enquiry"
+              className="w-full text-[#000000B2] text-[1rem] mt-1 block max-w-[25.0625rem] h-[2.5rem] rounded-md border border-gray-900 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            >
+              <option className="text-[#000000B2]">Purpose Of Enquiry</option>
+              <option value="Schedule a Phone Call">Schedule a Phone Call</option>
+              <option value="Schedule a Video Call">Schedule a Video Call</option>
+              <option value="Schedule a Site Visit (subject to Govt. laws)">Schedule a Site Visit</option>
+            </select>
+          </div>
+          <button className="w-[19.3125rem] h-[2.5rem] bg-[#A58850] text-[#ffffff] rounded-md m-auto block mt-[2.9375rem]">
+            Get Instant Callback
+          </button>
         </div>
-        <button className="w-[19.3125rem] h-[2.5rem] bg-[#A58850] text-[#ffffff] rounded-md m-auto block mt-[2.9375rem]">
-          Get Instant Callback
-        </button>
-      </div>
+      </form>
     </div>
   );
 };
