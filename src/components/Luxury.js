010568cce167b499a95d61e2d3236a7005e0abf0
Nikhil Jadhav <jnikhil652@gmail.com>
2023-03-13 16:08:39 +0530

    update

--- src/components/Luxury.js
+++ src/components/Luxury.js
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import axios from "axios";
 
 const Luxury = () => {
   const handleSubmit = async (e) => {
