e9f7201c848c598b35ee5a89886cb98a49610c82
Uday Balram <uday.balram@usdservices.in>
2023-03-20 13:54:56 +0530

    update

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -80,10 +80,15 @@ const Brand = () => {
         <h1 className="text-center font-bold text-[2rem] text-[#816A3D] font-poppins mb-[0.65125rem] mt-[0.8125rem]">
           NEIGHBOURHOOD
         </h1>
-        <p className="font-poppins font-medium text-[1rem] text-center">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s,Lorem Ipsum is simply du
+        <p className="font-poppins font-normal text-[1rem] text-center">
+          Welcome to 27grand project, our property is perfectly situated within
+          walking distance of major IT Parks, shopping centers, schools, and
+          hospitals, making it an ideal location for anyone looking for
+          convenient and comfortable living. Our project is situated in an area
+          that is continuing to grow and improve. There are several exciting
+          development plans in the works, including new shopping centers, parks,
+          and cultural attractions, making it an ideal location for anyone
+          looking to invest in their future.
         </p>
         <div className="flex gap-[2.5rem] flex-wrap justify-center mt-[3.17125rem]">
           <div
--- src/components/Luxury.js
+++ src/components/Luxury.js
@@ -2,7 +2,11 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import a1 from "../assets/a1.png";
+import a2 from "../assets/a2.png";
+import a3 from "../assets/a3.png";
+import a4 from "../assets/a4.png";
+import { Link } from "react-router-dom";
 const Luxury = () => {
   const slides = [
     {
@@ -49,83 +53,238 @@ const Luxury = () => {
   };
   return (
     <div>
-      <Slider {...settings}>
-        <div className="bg-cover h-[64.3rem] flex flex-col items-center justify-center w-screen bg-center max-md:h-[40rem]">
-          <img
-            className="h-[64.3rem] w-screen absolute max-md:h-[40rem]"
-            src="https://res.cloudinary.com/drjnmxyd5/image/upload/v1677750121/WhatsApp_Image_2023-03-01_at_4.05_4_rujwke.png"
-            alt=""
-          />
-          <div
-            className="w-[10rem] rounded-md hidden md:block relative left-[43%] top-[85%]"
-            style={{ backgroundColor: "#000000" }}
-          >
+      <div className="max-md:hidden">
+        <Slider {...settings}>
+          <div className="bg-cover h-[64.3rem] flex flex-col items-center justify-center w-screen bg-center max-md:h-[40rem]">
+            <img
+              className="h-[64.3rem] w-screen absolute max-md:h-[40rem]"
+              src={a1}
+              alt=""
+            />
             <div
-              className="w-[10rem] flex items-center justify-center h-[3.1875rem] rounded-md"
+              className="w-[10rem] rounded-md relative left-0 m-auto top-[85%] max-md:left-0 max-md:m-auto"
               style={{ backgroundColor: "#000000" }}
             >
-              <img
-                className="h-[1.9375rem]"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/768px-WhatsApp.svg.png?20220228223904"
-                alt=""
-              />
-              <p className="font-poppins font-bold" style={{ color: "white" }}>
-                WhatsApp
-              </p>
+              <div
+                className="w-[10rem] flex items-center justify-center h-[3.1875rem] rounded-md"
+                style={{ backgroundColor: "#000000" }}
+              >
+                <img
+                  className="h-[1.9375rem]"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/768px-WhatsApp.svg.png?20220228223904"
+                  alt=""
+                />
+                <p
+                  className="font-poppins font-bold"
+                  style={{ color: "white" }}
+                >
+                  WhatsApp
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="bg-cover h-[64.3rem] flex flex-col items-center justify-center w-screen bg-center max-md:h-[40rem]">
-          <img
-            className="h-[64.3rem] w-screen absolute max-md:h-[40rem]"
-            src="https://res.cloudinary.com/drjnmxyd5/image/upload/v1677750121/WhatsApp_Image_2023-03-01_at_4.05_4_rujwke.png"
-            alt=""
-          />
-          <div
-            className="w-[10rem] rounded-md hidden md:block relative left-[43%] top-[85%]"
-            style={{ backgroundColor: "#000000" }}
-          >
+          <div className="bg-cover h-[64.3rem] flex flex-col items-center justify-center w-screen bg-center max-md:h-[40rem]">
+            <img
+              className="h-[64.3rem] w-screen absolute max-md:h-[40rem]"
+              src={a2}
+              alt=""
+            />
             <div
-              className="w-[10rem] flex items-center justify-center h-[3.1875rem] rounded-md"
+              className="w-[10rem] rounded-md relative left-0 m-auto top-[85%] max-md:left-0 max-md:m-auto"
               style={{ backgroundColor: "#000000" }}
             >
-              <img
-                className="h-[1.9375rem]"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/768px-WhatsApp.svg.png?20220228223904"
-                alt=""
-              />
-              <p className="font-poppins font-bold" style={{ color: "white" }}>
-                WhatsApp
-              </p>
+              <div
+                className="w-[10rem] flex items-center justify-center h-[3.1875rem] rounded-md"
+                style={{ backgroundColor: "#000000" }}
+              >
+                <img
+                  className="h-[1.9375rem]"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/768px-WhatsApp.svg.png?20220228223904"
+                  alt=""
+                />
+                <p
+                  className="font-poppins font-bold"
+                  style={{ color: "white" }}
+                >
+                  WhatsApp
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="bg-cover h-[64.3rem] flex flex-col items-center justify-center w-screen bg-center max-md:h-[40rem]">
-          <img
-            className="h-[64.3rem] w-screen absolute max-md:h-[40rem]"
-            src="https://res.cloudinary.com/drjnmxyd5/image/upload/v1677750121/WhatsApp_Image_2023-03-01_at_4.05_4_rujwke.png"
-            alt=""
-          />
-          <div
-            className="w-[10rem] rounded-md hidden md:block relative left-[43%] top-[85%]"
-            style={{ backgroundColor: "#000000" }}
-          >
+          <div className="bg-cover h-[64.3rem] flex flex-col items-center justify-center w-screen bg-center max-md:h-[40rem]">
+            <img
+              className="h-[64.3rem] w-screen absolute max-md:h-[40rem]"
+              src={a3}
+              alt=""
+            />
             <div
-              className="w-[10rem] flex items-center justify-center h-[3.1875rem] rounded-md"
+              className="w-[10rem] rounded-md relative left-0 m-auto top-[85%] max-md:left-0 max-md:m-auto"
               style={{ backgroundColor: "#000000" }}
             >
-              <img
-                className="h-[1.9375rem]"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/768px-WhatsApp.svg.png?20220228223904"
-                alt=""
-              />
-              <p className="font-poppins font-bold" style={{ color: "white" }}>
-                WhatsApp
-              </p>
+              <div
+                className="w-[10rem] flex items-center justify-center h-[3.1875rem] rounded-md"
+                style={{ backgroundColor: "#000000" }}
+              >
+                <img
+                  className="h-[1.9375rem]"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/768px-WhatsApp.svg.png?20220228223904"
+                  alt=""
+                />
+                <p
+                  className="font-poppins font-bold"
+                  style={{ color: "white" }}
+                >
+                  WhatsApp
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </Slider>
+          <div className="bg-cover h-[64.3rem] flex flex-col items-center justify-center w-screen bg-center max-md:h-[40rem]">
+            <img
+              className="h-[64.3rem] w-screen absolute max-md:h-[40rem]"
+              src={a4}
+              alt=""
+            />
+            <div
+              className="w-[10rem] rounded-md relative left-0 m-auto top-[85%] max-md:left-0 max-md:m-auto"
+              style={{ backgroundColor: "#000000" }}
+            >
+              <div
+                className="w-[10rem] flex items-center justify-center h-[3.1875rem] rounded-md"
+                style={{ backgroundColor: "#000000" }}
+              >
+                <img
+                  className="h-[1.9375rem]"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/768px-WhatsApp.svg.png?20220228223904"
+                  alt=""
+                />
+                <p
+                  className="font-poppins font-bold"
+                  style={{ color: "white" }}
+                >
+                  WhatsApp
+                </p>
+              </div>
+            </div>
+          </div>
+        </Slider>
+      </div>
+      <div className="md:hidden">
+        <Slider {...settings}>
+          <div className="bg-cover h-[64.3rem] flex flex-col items-center justify-center w-screen bg-center max-md:h-[40rem]">
+            <img
+              className="h-[64.3rem] w-screen absolute max-md:h-[40rem]"
+              src="https://res.cloudinary.com/drjnmxyd5/image/upload/v1679299982/IMG-20230301-WA0002_hn07gx.jpg"
+              alt=""
+            />
+            <div
+              className="w-[10rem] rounded-md relative left-0 m-auto top-[85%] max-md:left-0 max-md:m-auto"
+              style={{ backgroundColor: "#000000" }}
+            >
+              <div
+                className="w-[10rem] flex items-center justify-center h-[3.1875rem] rounded-md"
+                style={{ backgroundColor: "#000000" }}
+              >
+                <img
+                  className="h-[1.9375rem]"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/768px-WhatsApp.svg.png?20220228223904"
+                  alt=""
+                />
+                <p
+                  className="font-poppins font-bold"
+                  style={{ color: "white" }}
+                >
+                  WhatsApp
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-cover h-[64.3rem] flex flex-col items-center justify-center w-screen bg-center max-md:h-[40rem]">
+            <img
+              className="h-[64.3rem] w-screen absolute max-md:h-[40rem]"
+              src="https://res.cloudinary.com/drjnmxyd5/image/upload/v1679299975/IMG-20230301-WA0022_ylnqpb.jpg"
+              alt=""
+            />
+            <div
+              className="w-[10rem] rounded-md relative left-0 m-auto top-[85%] max-md:left-0 max-md:m-auto"
+              style={{ backgroundColor: "#000000" }}
+            >
+              <div
+                className="w-[10rem] flex items-center justify-center h-[3.1875rem] rounded-md"
+                style={{ backgroundColor: "#000000" }}
+              >
+                <img
+                  className="h-[1.9375rem]"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/768px-WhatsApp.svg.png?20220228223904"
+                  alt=""
+                />
+                <p
+                  className="font-poppins font-bold"
+                  style={{ color: "white" }}
+                >
+                  WhatsApp
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-cover h-[64.3rem] flex flex-col items-center justify-center w-screen bg-center max-md:h-[40rem]">
+            <img
+              className="h-[64.3rem] w-screen absolute max-md:h-[40rem]"
+              src="https://res.cloudinary.com/drjnmxyd5/image/upload/v1679299970/IMG-20230301-WA0020_dnbabq.jpg"
+              alt=""
+            />
+            <div
+              className="w-[10rem] rounded-md relative left-0 m-auto top-[85%] max-md:left-0 max-md:m-auto"
+              style={{ backgroundColor: "#000000" }}
+            >
+              <div
+                className="w-[10rem] flex items-center justify-center h-[3.1875rem] rounded-md"
+                style={{ backgroundColor: "#000000" }}
+              >
+                <img
+                  className="h-[1.9375rem]"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/768px-WhatsApp.svg.png?20220228223904"
+                  alt=""
+                />
+                <p
+                  className="font-poppins font-bold"
+                  style={{ color: "white" }}
+                >
+                  WhatsApp
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-cover h-[64.3rem] flex flex-col items-center justify-center w-screen bg-center max-md:h-[40rem]">
+            <img
+              className="h-[64.3rem] w-screen absolute max-md:h-[40rem]"
+              src="https://res.cloudinary.com/drjnmxyd5/image/upload/v1679299965/IMG-20230301-WA0021_eswjyb.jpg"
+              alt=""
+            />
+            <div
+              className="w-[10rem] rounded-md relative left-0 m-auto top-[85%] max-md:left-0 max-md:m-auto"
+              style={{ backgroundColor: "#000000" }}
+            >
+              <div
+                className="w-[10rem] flex items-center justify-center h-[3.1875rem] rounded-md"
+                style={{ backgroundColor: "#000000" }}
+              >
+                <img
+                  className="h-[1.9375rem]"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/768px-WhatsApp.svg.png?20220228223904"
+                  alt=""
+                />
+                <p
+                  className="font-poppins font-bold"
+                  style={{ color: "white" }}
+                >
+                  WhatsApp
+                </p>
+              </div>
+            </div>
+          </div>
+        </Slider>
+      </div>
       <div className="container h-[13.9375rem] m-auto max-xl:h-[27.5625rem]">
         <div className="gap-[0.625rem] flex justify-center flex-wrap font-poppins mt-[1.625rem]">
           <h1 className="font-semibold text-[1rem] font-poppins md:hidden">
@@ -168,6 +327,16 @@ const Luxury = () => {
         </div>
         <button className="w-[19.3125rem] h-[2.5rem] bg-[#A58850] text-[#ffffff] rounded-md m-auto block mt-[2.9375rem]">
           Get Instant Callback
+        </button>
+      </div>
+      <div className="bg-[#ffffff] h-[4rem] w-full flex md:hidden fixed bottom-[10%] items-center justify-evenly">
+        <Link to="/enquireform">
+          <button className="w-[9rem] h-[2.5rem] bg-[#A58850] text-[#ffffff]  block">
+            Equire Now
+          </button>
+        </Link>
+        <button className="w-[9rem] h-[2.5rem] bg-[#A58850] text-[#ffffff] block">
+          Call Now
         </button>
       </div>
     </div>
